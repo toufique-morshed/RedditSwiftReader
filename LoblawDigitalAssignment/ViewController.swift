aa2744d724e86e83c2ab1b81f5e57553060b6b94
Morshed <morshed@csebuet.org>
2020-05-12 15:59:31 -0500

    [Issue]: Create Project [Solution]: Project Created using the standard procedure [Bug ID]: 01
    Signed-off-by: Morshed <morshed@csebuet.org>

---
+++ LoblawDigitalAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoblawDigitalAssignment
+//
+//  Created by Tamal, Md Toufique Morshed on 5/12/20.
+//  Copyright © 2020 Tamal, Md Toufique Morshed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
