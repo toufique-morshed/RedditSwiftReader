d562699604362a2eee65bb56131028183b21440f
Morshed <morshed@csebuet.org>
2020-05-12 22:59:51 -0500

    [Issue]: Program failure. Response Not found for reddit API [Solution]: Add dummy response model [Bug ID]: 03
    Signed-off-by: Morshed <morshed@csebuet.org>

---
+++ LoblawDigitalAssignment/Classes/Models/Network/Response/LDARedditResponse.swift
@@ -0,0 +1,21 @@
+//
+//  LDARedditResponse.swift
+//  LoblawDigitalAssignment
+//
+//  Created by Tamal, Md Toufique Morshed on 5/12/20.
+//  Copyright © 2020 Tamal, Md Toufique Morshed. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+
+class LDARedditResponse: Mappable {
+    
+    required init?(map: Map) {
+
+    }
+    
+    func mapping(map: Map) {
+    }
+}
